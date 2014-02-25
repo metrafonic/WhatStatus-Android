d157fab15fbfb341cea0641137289290705fb2f8
Mathias Hedberg <hedberg.mathias@gmail.com>
2014-02-25 21:08:49 +0100

    New start

---
+++ whatstatus.checker/src/main/java/com/example/whatstatus/checker/Fragment_Status.java
@@ -0,0 +1,21 @@
+package com.example.whatstatus.checker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mathias on 25.02.14.
+ */
+public class Fragment_Status extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_status, container, false);
+
+        return rootView;
+    }
+
+}
